4d65a84a90a07011765346d53f0ae077b57a1b38
DESKTOP-CCEPJV7\user <781686379@qq.com>
2019-02-26 14:47:05 +0800

    thread

--- ribbon-consumer/src/main/java/com/ribbonconsumer/thirdparty/sort/Sort.java
+++ ribbon-consumer/src/main/java/com/ribbonconsumer/thirdparty/sort/Sort.java
@@ -1,5 +1,9 @@
 package com.ribbonconsumer.thirdparty.sort;
 
+import com.ribbonconsumer.base.util.DateUtils;
+
+import java.util.Arrays;
+
 /**
  * @author qian.wang
  * @description 排序算法
@@ -8,17 +12,49 @@ package com.ribbonconsumer.thirdparty.sort;
 public class Sort {
 
     //冒泡排序
-    public static int[] orderByBubble(int[] a){
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a.length-i-1;j++){
-                if(a[j]>a[j+1]){
-                    int temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
+    public static int[] orderByBubble(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
         return a;
+    }
+
+    //选择排序
+    public static int [] selectSort(int[] array) {
+        int tmp = 0;
+        int minIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    tmp = array[minIndex];
+                    array[minIndex] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int a[]={1,3,9,5,7,2,5,7,8,3,2};
+        long start=System.currentTimeMillis();
+//        System.out.println(Arrays.toString(orderByBubble(a)));
+        System.out.println(Arrays.toString(selectSort(a)));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(System.currentTimeMillis()-start);
+//        System.out.println(Arrays.toString(selectSort(a)));
     }
 
 
--- ribbon-consumer/src/main/java/com/ribbonconsumer/thirdparty/thread/ThreadBox.java
+++ ribbon-consumer/src/main/java/com/ribbonconsumer/thirdparty/thread/ThreadBox.java
@@ -6,6 +6,12 @@ package com.ribbonconsumer.thirdparty.thread;
  * @date 2019/2/20
  */
 /*
+问：sleep和wait方法的区别？
+答：首先sleep()是Thread类的静态方法，wait()是object的方法，这在本质上还是有区别的，sleep不能改变对象的机锁，所以当一个Synchronized块中调用Sleep()方法，线程虽然休眠了,
+但是对象的机锁并木有被释放，其他线程无法访问这个对象，即使睡着了也同样持有该对象锁。在sleep()结束休眠后，该线程并不一定会立即执行，因为其他线程可能正在执行而且没有被调度为放弃执行，
+除非线程有更高的优先级，对于CPU资源来说，不管哪个方式暂停的线程，都表示暂时不需要CPU时间，OS会将执行时间分配给别的线程，区别是使用wait()方法后，释放了该对象的机锁【暂时失去】，
+需要使用notify/notifyall来唤醒等待池中的线程才能重新获取CPU执行时间，wait()方法必须放在Synchornized 代码块中，否则会抛出异常。
+---------------------------------------------------------------------------------------------
  生产者和消费者模式在生活当中随处可见，它描述的是协调与协作的关系。比如一个人正在准备食物（生产者），而另一个人正在吃（消费者），
  他们使用一个共用的桌子用于放置盘子和取走盘子，生产者准备食物，如果桌子上已经满了就等待，消费者（那个吃的）等待如果桌子空了的话。这里桌子就是一个共享的对象。
  我们看这样一个例子：
@@ -18,7 +24,7 @@ public class ThreadBox {
     private int orange = 0;
 
     public synchronized void create() {
-        while (orange==10){
+        while (orange == 10) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -30,8 +36,8 @@ public class ThreadBox {
         notify();
     }
 
-    public synchronized void use(){
-        while (orange==0){
+    public synchronized void use() {
+        while (orange == 0) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -46,13 +52,13 @@ public class ThreadBox {
 
     //消费者生产者测试
     public static void main(String[] args) {
-        ThreadBox box= new ThreadBox();
+        ThreadBox box = new ThreadBox();
 
-        Consumer con= new Consumer(box);
-        Producer pro= new Producer(box);
+        Consumer con = new Consumer(box);
+        Producer pro = new Producer(box);
 
-        Thread t1= new Thread(con);
-        Thread t2= new Thread(pro);
+        Thread t1 = new Thread(con);
+        Thread t2 = new Thread(pro);
 
         t1.start();
         t2.start();
